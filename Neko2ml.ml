9e063d9024a6abfb048c85fdaf39e6a32cccfb21
ncannasse <ncannasse@motion-twin.com>
2012-09-26 18:33:15 +0200

    a bit more work

--- Neko2ml.ml
+++ Neko2ml.ml
@@ -28,5 +28,120 @@ and vfunction =
 	| Fun5 of (value -> value -> value -> value -> value -> value)
 	| FunVar of (value list -> value)
 
-let acc = ref VNull
+exception Runtime of value
+
+exception Return of value
+
+let empty_env = [||]
+
+let vtrue = VBool true
+
+let vfalse = VBool false
+
+let throw v =
+	raise (Runtime v)
+	
+let return v =
+	raise (Return v)
+	
+let todo str =
+	failwith str
+	
+(* ---- TODO : Array.unsafe ! *)
+
+let get_env (env : value array) i =
+	Array.get env i
+	
+let set_env (env : value array) i v =
+	Array.set env i v
+	
+let get_index a i =
+	match a with
+	| VArray a -> if i >= 0 && i < Array.length a then Array.unsafe_get a i else VNull
+	| VObject _ -> assert false (* not supported *)
+	| _ -> throw (VString "Invalid array access")
+
+let set_index a i v =
+	match a with
+	| VArray a -> if i >= 0 && i < Array.length a then Array.unsafe_set a i v
+	| VObject _ -> assert false (* not supported *)
+	| _ -> throw (VString "Invalid array access")
+	
+let do_call0 f =
+	match f with
+	| VFunction (Fun0 f) -> f()
+	| VFunction (FunVar f) -> f []
+	| _ -> throw (VString "Invalid call")
+	
+let do_call1 f a =
+	match f with
+	| VFunction (Fun1 f) -> f a
+	| VFunction (FunVar f) -> f [a]
+	| _ -> throw (VString "Invalid call")
+	
+let do_call2 f a b =
+	match f with
+	| VFunction (Fun2 f) -> f a b
+	| VFunction (FunVar f) -> f [a;b]
+	| _ -> throw (VString "Invalid call")
+	
+let do_call3 f a b c =
+	match f with
+	| VFunction (Fun3 f) -> f a b c
+	| VFunction (FunVar f) -> f [a;b;c]
+	| _ -> throw (VString "Invalid call")
+	
+let do_call4 f a b c d =
+	match f with
+	| VFunction (Fun4 f) -> f a b c d
+	| VFunction (FunVar f) -> f [a;b;c;d]
+	| _ -> throw (VString "Invalid call")
+	
+let do_call5 f a b c d e =
+	match f with
+	| VFunction (Fun5 f) -> f a b c d e
+	| VFunction (FunVar f) -> f [a;b;c;d;e]
+	| _ -> throw (VString "Invalid call")
+
+let do_add a b =
+	match a, b with
+	| VInt a, VInt b -> VInt (a + b)
+	| VFloat a, VFloat b -> VFloat (a +. b)
+	(* no support for polymorphism *)
+	| _ -> throw (VString "+")
+	
+let do_sub a b =
+	match a, b with
+	| VInt a, VInt b -> VInt (a - b)
+	| VFloat a, VFloat b -> VFloat (a -. b)
+	(* no support for polymorphism *)
+	| _ -> throw (VString "+")
+
+let do_mult a b =
+	match a, b with
+	| VInt a, VInt b -> VInt (a * b)
+	| VFloat a, VFloat b -> VFloat (a *. b)
+	(* no support for polymorphism *)
+	| _ -> throw (VString "+")
+		
+let do_div a b =
+	match a, b with
+	| VInt a, VInt b -> VFloat (float_of_int a /. float_of_int b)
+	| VFloat a, VFloat b -> VFloat (a /. b)
+	(* no support for polymorphism *)
+	| _ -> throw (VString "+")
+
+let do_compare a b =
+	if a == b then
+		0
+	else match a, b with
+	| VInt a, VInt b -> if a = b then 0 else if a < b then -1 else 1
+	| VFloat a, VFloat b -> if a = b then 0 else if a < b then -1 else 1
+	| VString a, VString b -> if a = b then 0 else if a < b then -1 else 1
+	| VBool a, VBool b -> if a = b then 0 else if a then 1 else -1
+	| _ -> assert false
+
+let is_true = function
+	| VBool true -> true
+	| _ -> false
 
